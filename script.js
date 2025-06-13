ef4beb9b1edf125d3eb9b6459859bc5c98e12c2d
Faizal Hassan <faizal19@gmail.com>
2025-06-13 16:53:24 +0500

    --

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 const addNoteBtn = document.querySelector('.Add-Note-Btn');
 const addNoteModal = document.querySelector('.addNoteModal');
 const closeNoteBtn = document.querySelector('.addNoteModal i');
+const overlay = document.querySelector('.overlay')
 
 // Add Note Popup Modal 
 addNoteBtn.addEventListener('click', function () {
-    addNoteModal.classList.add('show')
+    addNoteModal.classList.add('show');
+    overlay.classList.add('show')
 })
 //Close Popup Modal
 closeNoteBtn.addEventListener('click', function () {
     addNoteModal.classList.remove('show')
+    overlay.classList.remove('show')
 })
