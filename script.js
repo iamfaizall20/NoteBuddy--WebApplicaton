3086f6d9fe535c5206d398918c93b0fb92c522da
Faizal Hassan <faizal19@gmail.com>
2025-06-13 18:18:03 +0500

    ---

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const addNoteBtn = document.querySelector('.Add-Note-Btn');
 const addNoteModal = document.querySelector('.addNoteModal');
 const closeNoteBtn = document.querySelector('.addNoteModal i');
 const overlay = document.querySelector('.overlay')
+const saveNoteBtn = document.querySelector('.saveNoteBtn')
 
 // Add Note Popup Modal 
 addNoteBtn.addEventListener('click', function () {
@@ -10,7 +11,35 @@ addNoteBtn.addEventListener('click', function () {
     overlay.classList.add('show')
 })
 //Close Popup Modal
-closeNoteBtn.addEventListener('click', function () {
+function closeNoteModal() {
     addNoteModal.classList.remove('show')
     overlay.classList.remove('show')
+}
+
+
+saveNoteBtn.addEventListener('click', function () {
+    const Title = document.getElementById('title-field').value
+    const Description = document.getElementById('description-field').value
+
+    if (Title === "" || Description === "") {
+        alert("Fill Field")
+        return;
+    }
+    const NoteDiv = document.createElement('div')
+    NoteDiv.classList.add('note')
+
+    const NoteTitle = document.createElement('h2')
+    const NoteDescription = document.createElement('p')
+
+    NoteDescription.textContent = Description;
+    NoteTitle.textContent = Title;
+
+    NoteDiv.appendChild(NoteTitle)
+    NoteDiv.appendChild(NoteDescription)
+    document.querySelector('.notes-section').appendChild(NoteDiv);
+
+    Title.value = '';
+    Description.value = '';
+
+    closeNoteModal();
 })
